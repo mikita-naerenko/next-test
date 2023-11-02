88bf392fb7b9675594e944b8ffab65c197604d2a
Mikita <tetropak555666@gmail.com>
2023-11-02 17:48:31 +0900

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import styles from './ui/home.module.css';
-import { lusitana } from './ui/fonts.ts'
+import { lusitana } from '@/app/ui/fonts.ts'
 import Image from 'next/image';
 
 import Link from 'next/link';
